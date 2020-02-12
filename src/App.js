d6b28ac841955a74472a87166b32096220272602
ajiron <ajiron@liveperson.com>
2020-02-11 21:28:39 -0800

    disabled stale data fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Routes from './routes';
+import { ReactQueryConfigProvider } from 'react-query';
+
+const ReactQueryConfig = {
+  refetchAllOnWindowFocus: false
+};
 
 export default () => (
   <div className="App">
-    <Routes />
+    <ReactQueryConfigProvider config={ReactQueryConfig}>
+
+      <Routes />
+    </ReactQueryConfigProvider>
   </div>
 );
